435be290092676932bea6afd553fa3d81417025d
eujin <kstar1996@gmail.com>
2019-05-28 14:48:33 +0900

    discovery acitivity in discovery fragment

--- src/Android/app/src/main/java/com/example/flixtyle/DiscoveryActivity.java
+++ src/Android/app/src/main/java/com/example/flixtyle/DiscoveryActivity.java
@@ -1,41 +1,47 @@
 package com.example.flixtyle;
 
-import android.content.Intent;
+
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+
 import com.lorentzos.flingswipe.SwipeFlingAdapterView;
+import java.util.ArrayList;
+
 
 import java.util.ArrayList;
 
-public class DiscoveryActivity extends AppCompatActivity {
+
+public class DiscoveryActivity extends Fragment {
 
     private ArrayList<String> al;
     private ArrayAdapter<String> arrayAdapter;
     private int i;
 
-    private FirebaseAuth mAuth;
-
+    public DiscoveryActivity() {
+        // Required empty public constructor
+    }
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.discovery);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        final Context contextRegister = container.getContext();
+        View view = inflater.inflate(R.layout.activity_discovery, container, false);
 
 
         al = new ArrayList<>();
@@ -48,9 +54,9 @@ public class DiscoveryActivity extends AppCompatActivity {
         al.add("7");
         al.add("8");
 
-        arrayAdapter = new ArrayAdapter<>(this, R.layout.item, R.id.name_item, al );
+        arrayAdapter = new ArrayAdapter<>(getActivity(), R.layout.item, R.id.name_item, al);
 
-        SwipeFlingAdapterView flingContainer=(SwipeFlingAdapterView) findViewById(R.id.frame);
+        SwipeFlingAdapterView flingContainer = (SwipeFlingAdapterView)view.findViewById(R.id.frame);
 
         flingContainer.setAdapter(arrayAdapter);
         flingContainer.setFlingListener(new SwipeFlingAdapterView.onFlingListener() {
@@ -67,14 +73,13 @@ public class DiscoveryActivity extends AppCompatActivity {
                 //Do something on the left!
                 //You also have access to the original object.
                 //If you want to use it just cast it (String) dataObject
-                Toast.makeText(DiscoveryActivity.this, "left",Toast.LENGTH_SHORT ).show();
+                Toast.makeText(contextRegister, "left", Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onRightCardExit(Object dataObject) {
-                Toast.makeText(DiscoveryActivity.this, "right",Toast.LENGTH_SHORT ).show();
+                Toast.makeText(contextRegister, "right", Toast.LENGTH_SHORT).show();
             }
-
 
 
             @Override
@@ -98,55 +103,13 @@ public class DiscoveryActivity extends AppCompatActivity {
         flingContainer.setOnItemClickListener(new SwipeFlingAdapterView.OnItemClickListener() {
             @Override
             public void onItemClicked(int itemPosition, Object dataObject) {
-                Toast.makeText(DiscoveryActivity.this, "clicked",Toast.LENGTH_SHORT ).show();
+                Toast.makeText(contextRegister, "clicked", Toast.LENGTH_SHORT).show();
             }
         });
-
+        return view;
     }
-
-    public void checkUserSex(){
-        final FirebaseUser user=FirebaseAuth.getInstance().getCurrentUser();
-        DatabaseReference femaleDb= FirebaseDatabase.getInstance().getReference().child("Users").child("Female");
-        femaleDb.addChildEventListener(new ChildEventListener() {
-            @Override
-            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-                if(dataSnapshot.getKey().equals(user.getUid())){
-
-                }
-            }
-
-            @Override
-            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
-            }
-
-            @Override
-            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
     }
-
-    public void logoutUser(View view){
-        mAuth.signOut();
-        Intent intent=new Intent(DiscoveryActivity.this, LoginActivity.class);
-        startActivity(intent);
-        finish();
-        return;
-
-    }
-
-
 }
-
-
