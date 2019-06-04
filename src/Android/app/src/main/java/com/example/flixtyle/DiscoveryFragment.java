4cfd2753f80619cffea3abcd2719adacd66d264e
eujin <kstar1996@gmail.com>
2019-06-04 17:25:10 +0900

    .

--- src/Android/app/src/main/java/com/example/flixtyle/DiscoveryFragment.java
+++ src/Android/app/src/main/java/com/example/flixtyle/DiscoveryFragment.java
@@ -16,6 +16,29 @@ import android.widget.Toast;
 import com.lorentzos.flingswipe.SwipeFlingAdapterView;
 import java.util.ArrayList;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.lorentzos.flingswipe.SwipeFlingAdapterView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 import java.util.ArrayList;
 
@@ -113,6 +136,18 @@ public class DiscoveryFragment extends Fragment {
         void onFragmentInteraction(Uri uri);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
