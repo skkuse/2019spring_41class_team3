3693beae60de88443d3546067d89794057a1bf20
Takuuvakuutus tuki <tuki@takuuvakuutus.fi>
2019-06-25 04:18:37 +0900

    Recommendation system now interfaces with live data and application

--- src/Backend/routes/dev.js
+++ src/Backend/routes/dev.js
@@ -27,45 +27,5 @@ router.get('/test', function (req, res) {
 
 
 
-// Python proces promise creation
-function runPy(){
-  return new Promise(async function(resolve, reject){
-        let options = {
-        mode: 'text',
-        pythonOptions: ['-u'],
-        scriptPath: 'recommendation_system/',//Path to your script
-       };
-        await PythonShell.run('rec.py', options, function (err, results) { 
-        if (err) throw err;
-    resolve(1)
-   });
- })
-} 
-
-/* Reads from recommendation_system/recommendations.csv and updates
-  every user's recommendations field in firebase
-*/
-router.get('/recommendations', function (req, res) {
-  runPy().then(function (val) {
-    fs.createReadStream('recommendation_system/recommendations.csv')  
-    .pipe(csv())
-    .on('data', (row) => {
-        if(row.length > 1) {
-          userId = row[0];
-          row.shift();
-          req.db.ref('/Users/' + userId).update({recommendations: row}, function(error){
-            if (error){
-              console.log(error);
-            } else {
-              console.log("GET /dev/recommendation for user " + userId + " : Success with values: " + row + "\n");
-            }
-          });
-        }
-    })
-    .on('end', () => {
-    });
-    res.send("HTTP GET Request");
-  })
-});
 
 module.exports = router;
